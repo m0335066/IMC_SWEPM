c6cccd66b20b291571cc0912f8bda90f9d9c6f3c
m0335 <20IMC10288@fh-krems.ac.at>
2021-11-18 11:19:48 +0100

    set up webpack project with JEST

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -2,5 +2,5 @@ import {Calculator} from "./calculator";
 
 test("adding 2 integers", () => {
     const calc = new Calculator();
-    expect(calc.add(1,1)).tobe(2);
+    expect(calc.add(1,1)).toBe(2);
 });
